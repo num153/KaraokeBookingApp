b4c10acc9479f4feb758c35a1564f3cbeacd110d
Nam Phuong <dangphuongnam.hvt@gmail.com>
2025-12-21 13:05:12 +0700

    cài sqlalchemy

--- karaokeapp/__init__.py
+++ karaokeapp/__init__.py
@@ -1,4 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 
 app = Flask(__name__)
+# cấu hình sqlalchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:12345678@localhost/labdb?charset=utf8mb4'
+
+
+db=SQLAlchemy(app=app)
